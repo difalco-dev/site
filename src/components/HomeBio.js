16bfd5d9af0692533ede663881cf85e5cc462d01
Anthony DiFalco <difalco.dev@gmail.com>
2020-04-28 20:40:50 -0400

    last update

--- src/components/HomeBio.js
+++ src/components/HomeBio.js
@@ -7,12 +7,14 @@ export default function HomeBio() {
         <div>
             <Typography variant="subtitle1"><b>About</b></Typography>
             <Typography variant="body2" paragraph>
-                Hi! I'm an application developer and business process designer currently working for
-                    Grant Thornton LLP at their Chicago headquarters. I'm passionate about bringing
-                    cutting-edge technology into antiquated business processes. I've been coding for 
+                    I'm a technology consultant with experience in the tax and software industries. Currently, 
+                    I design and implement custom business software solutions for internal teams and external 
+                    clients at Grant Thornton. I leverage Electron, Node, and React to provide transformative 
+                    business process tools. Passionate about disruptive technologies in untapped markets and 
+                    bridging the gap between business and technology.  I've been coding for 
                     five <b>(long)</b> years and have experience with a wide range of technology stacks
-                    and concepts. My professional work mainly revolves around Node, Electron, and  React, 
-                    but I also have notable experience with Python, Git, React Native, UNIX, and C++.
+                    and concepts. Although my professional work mainly revolves around Node, Electron, and  React, 
+                    I have notable experience with Python, Git, React Native, UNIX, and C++.
             </Typography>
         </div>
     );
